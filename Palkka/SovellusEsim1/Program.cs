986176d9977ce83c90f8634d7b9f5d9d47741053
TerhiMannermaa <89631565+TerhiMannermaa@users.noreply.github.com>
2021-09-30 14:55:03 +0300

    Add files via upload

---
+++ Palkka/SovellusEsim1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SovellusEsim1
+{
+    class Program
+    {
+        static string ohje = "0.Lopetus\n1.Lisää\n2.Listaa\n3.Palkanmaksupäivät\n4.Poista työntekijä";
+
+        static void Main(string[] args)
+        {
+            Sovellus app = new Sovellus();
+
+            Console.WriteLine(ohje);
+            string komento = Console.ReadLine();
+            while (!komento.StartsWith("0"))   // ! tarkoittaa NOT
+            {
+                switch (komento)
+                {
+                    case "1":
+                        Console.WriteLine("Anna työntekijän nimi");
+                        var nimi = Console.ReadLine();
+
+                        Console.WriteLine("Anna palkkatiedot");
+                        var palkkatiedot = Console.ReadLine();
+
+                        Console.WriteLine("Anna ennakonpidätysprosentti");
+                        var Ennakonpidätys = Console.ReadLine();
+
+                        Console.WriteLine("Anna palkanmaksupäivä");
+                        var Palkanmaksupäivä = Console.ReadLine();
+
+
+
+                        var työntekijä = new Työntekijä(palkkatiedot,nimi,Ennakonpidätys,Palkanmaksupäivä);                        
+
+                        app.LisääTyöntekijä(työntekijä);
+                        break;
+
+                    case "2":
+                        Console.WriteLine(app.ListaaTyöntekijät());
+                        break;
+
+                    default:        // kaikissa muissa tapauksissa
+                        break;
+
+                    case "3":
+                        Console.WriteLine(app.ListaaPalkkapäivät());
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Valitse poistettava työntekijä");
+                        Console.WriteLine(app.ListaaTyöntekijät());
+                        var poistettava = Console.ReadLine();
+                        Console.WriteLine(app.PoistaTyöntekijä());
+                        
+                        break;
+                }
+                Console.WriteLine(ohje);
+                komento = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Palkka/SovellusEsim1/Sovellus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SovellusEsim1
+{
+    //dataluokat
+    record Työntekijä(string PalkkaTieto, string Nimi, string Ennakonpidätys, string Palkanmaksupäivä);
+    record Palkkatieto(string Palkkatietoja, Työntekijä Omistaja);
+
+    // hallintaluokka
+    class Sovellus
+    {
+        //listat (huom! p.o. public tallentamista varten)
+        
+        public List<Palkkatieto> Palkkatieto = new List<Palkkatieto>();
+        public List<Työntekijä> Työntekijät = new List<Työntekijä>();
+        
+        
+
+        /// <summary>
+        /// Lisää henkilö
+        /// </summary>
+        /// <param name="työntekijä">Lisättävä henkilö</param>
+        public void LisääTyöntekijä(Työntekijä työntekijä)
+        {
+            Työntekijät.Add(työntekijä);
+        }
+        //public void PoistaTyöntekijä(Työntekijä työntekijä)
+            public void PoistaTyöntekijä(String Nimi) 
+        {
+            var t = Työntekijät.Find(x => x.Nimi == "abc");
+
+            Työntekijät.Remove(t);
+        }
+
+        public string ListaaTyöntekijät()
+        {
+            string retval = "";
+            foreach (var item in Työntekijät)
+            {
+                retval += $"{item.PalkkaTieto} {item.Nimi} {item.Ennakonpidätys} {item.Palkanmaksupäivä}\n";
+            }
+            return retval;
+        }
+        public string ListaaPalkkapäivät()
+        {
+            string retval = "";
+            foreach (var item in Työntekijät)
+            {
+                retval += $"{item.Nimi} {item.Palkanmaksupäivä}\n";
+            }
+            return retval;
+        }
+
+    }
+}
